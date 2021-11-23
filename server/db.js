4f52e2892e67567b548ced56956adfb0a794bb7f
gran1um <65013672+gran1um@users.noreply.github.com>
2021-11-23 20:35:24 +0300

    1.0

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize')
+
+module.exports = new Sequelize(
+    process.env.DB_NAME, //db name
+    process.env.DB_USER, //db user
+    process.env.DB_PASSWORD, //db password
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+)
